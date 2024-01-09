72ac1a1a4bae0bae827d1461fbd836c3bbd7f49c
DooHwanKim92 <rmfpsvldj01@naver.com>
2024-01-09 14:11:47 +0900

    t1

---
+++ src/main/java/com/sbtest/test2ex/Test2exApplication.java
@@ -0,0 +1,15 @@
+package com.sbtest.test2ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2exApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2exApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/sbtest/test2ex/Test2exApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtest.test2ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2exApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
